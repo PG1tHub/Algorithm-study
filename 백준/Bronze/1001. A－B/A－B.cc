315a9cf63ab977b4ad134c941b18a73b7b289d2b
G1 <94522835+PG1tHub@users.noreply.github.com>
2022-06-30 17:18:47 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    int A;
+    int B;
+    cin >> A;
+    cin >> B;
+    if(A>0,B,10){
+    cout << A-B <<endl;
+    }
+    return 0;
+}
