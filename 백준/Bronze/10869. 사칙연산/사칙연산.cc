e1ae8b2f49e7160558696f172f311138b8cc0809
G1 <94522835+PG1tHub@users.noreply.github.com>
2022-06-30 18:17:29 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ int A,B;
+    cin >> A;
+    cin >> B;
+    if(A>=1 && B<=10000){
+        cout << A+B << endl;
+        cout << A-B << endl;
+        cout << A*B << endl;
+        cout << A/B << endl;
+        cout << A%B << endl;
+    }
+    return 0;
+}
