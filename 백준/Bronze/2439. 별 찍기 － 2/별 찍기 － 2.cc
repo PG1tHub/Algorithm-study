69f60c686a7a14ebf93f1e185c864efcaeddda9a
G1 <94522835+PG1tHub@users.noreply.github.com>
2022-07-01 20:09:25 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cin >>a;
+    for(int i=0;i<a;i++){
+        for(int j=a-2;j>=i;j--){
+            cout<<" ";
+        }
+        for(int k=0;k<=i;k++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
