b5105baeedcc01ac7287d8d5a03a101c96752470
G1 <94522835+PG1tHub@users.noreply.github.com>
2023-11-24 14:52:18 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+     
+    string str;
+    cin >> str;
+    
+    cout << str.length() << endl;
+
+    return 0;
+}
