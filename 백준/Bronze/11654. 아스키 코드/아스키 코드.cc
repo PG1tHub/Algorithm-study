7da9eb56e5327c00fde9850d6bfb0127e014436d
G1 <94522835+PG1tHub@users.noreply.github.com>
2023-11-24 15:14:45 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+     
+    char C;
+    cin >> C;
+    
+    int a = static_cast<int>(C);
+    cout << a <<endl;
+    
+    return 0;
+}
