c659d76da8fe6af670b8e52f1565714374edcf7c
G1 <94522835+PG1tHub@users.noreply.github.com>
2024-01-02 18:16:49 +0900

    Update 수 찾기.cc

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,33 +1,28 @@
-#include <iostream>
-#include <vector>
 #include <algorithm>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <cmath>
 using namespace std;
 
 int main() {
-    int N, M;
-    scanf("%d", &N);
-    
-    vector<long long int> v(N);
-    for (int i = 0; i < N; ++i) {
-        scanf("%lld", &v[i]);
+    int N,M;
+    int a;
+    int t;
+    vector <long long int> v;
+    cin >> N;
+    for(int i = 0; i<N; i++){
+        cin >>a;
+        v.push_back(a);
     }
-    
-    sort(v.begin(), v.end());
-    
-    scanf("%d", &M);
-    while (M--) {
-        int t;
+    sort(v.begin(),v.end());
+    cin >> M;
+    for(int i =0; i<M; i++){
         scanf("%d", &t);
-        
-        auto lower = lower_bound(v.begin(), v.end(), t);
-        auto upper = upper_bound(v.begin(), v.end(), t);
-        
-        if (lower != v.end() && *lower == t) {
-            printf("1\n");
-        } else {
-            printf("0\n");
-        }
+        int target = t;
+        bool found = binary_search(v.begin(), v.end(), target);
+        printf("%d\n", found);
     }
-    
     return 0;
 }
+            
