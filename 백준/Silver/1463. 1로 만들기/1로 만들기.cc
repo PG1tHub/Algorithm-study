6c35506bf108253054cdd4992060fc882d171177
G1 <94522835+PG1tHub@users.noreply.github.com>
2024-01-13 13:24:48 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    vector<int> dp(x + 1,0);
+    for(int i = 2; i<=x; i++){
+        dp[i] = dp[i-1] + 1;
+        if((i%3) == 0){
+            dp[i] = min(dp[i],dp[i/3] + 1);
+        }
+        if((i%2) == 0){
+            dp[i] = min(dp[i],dp[i/2] + 1);
+        }
+    }
+    cout << dp[x] << '\n';
+    
+    
+    
+    return 0;
+}
+
