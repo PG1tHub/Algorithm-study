68d6ca3152b0e51627c540828daa99acc3ea8e69
G1 <94522835+PG1tHub@users.noreply.github.com>
2025-10-21 13:26:31 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -0,0 +1,28 @@
+#include <algorithm>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int n;
+    int dp[41] = {0, 1, 1};
+    for(int i = 3; i<41; i++){
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    
+    cin >> n;
+    for(int i = 0; i< n; i++){
+        int a;
+        cin >> a;
+        if(a == 0){
+            cout << 1 << ' ' << 0 << '\n';
+        }
+        else if(a == 1){
+            cout << 0 << ' ' << 1 << '\n';
+        }
+        else{
+            cout << dp[a-1] << ' ' << dp[a] << '\n';
+        }
+    }
+}
